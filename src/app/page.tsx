85577678b09a6f7e3e964b9a278ec592c2284b49
Andrew Zhou <andrewzhou16@gmail.com>
2023-12-21 22:47:10 -0800

    Add: Create page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Button from './components/Button';
 import Input from './components/Input';
 import styles from './page.module.scss';
@@ -7,9 +8,11 @@ const Home = () => {
     <main className={styles.main}>
       <div className={styles.title}>TOSS-IT!</div>
       <Input className={styles.roomCodeInput} placeholder="Room Code" />
-      <Button className={styles.toTeacherMode} fill="secondary">
-        To Teacher Mode
-      </Button>
+      <Link href="/create">
+        <Button className={styles.toTeacherMode} fill="secondary">
+          To Teacher Mode
+        </Button>
+      </Link>
       <div className={styles.box} />
     </main>
   );
