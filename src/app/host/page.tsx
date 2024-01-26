20f6183f59ec0697d05871f5478d469702b0de0d
Sophia Zhang <sophia.p.zhang@gmail.com>
2024-01-25 17:17:43 -0800

    Tie to original room creation socket function + callback

--- src/app/host/page.tsx
+++ src/app/host/page.tsx
@@ -1,13 +1,28 @@
+'use client';
+
 import Link from 'next/link';
 import Button from '../components/Button';
 import styles from './page.module.scss';
+import { socketAtom } from '../../context/socket';
+import { useAtom } from 'jotai';
 
 const HostRoom = () => {
+  const [socket, setSocket] = useAtom(socketAtom);
+
+  const handleCreateRoom = e => {
+    e.preventDefault();
+    console.log('host room clicked');
+    socket.emit('createGame', response => {
+      console.log(response.ok);
+      console.log(response.room);
+    });
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles.title}>TOSS-IT!</div>
       <div className={styles.forTeachers}>FOR TEACHERS</div>
-      <Button>Host Room</Button>
+      <Button onClick={e => handleCreateRoom(e)}>Host Room</Button>
       <Link href="/">
         <Button className={styles.toStudentMode} fill="secondary">
           To Student Mode
