3236ae307341d9b7812fbe88fd2beb213d25b541
Andrew Zhou <andrewzhou16@gmail.com>
2024-04-15 00:58:13 -0700

    Add: Use env url

--- src/context/socket.tsx
+++ src/context/socket.tsx
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 import type { Socket } from 'socket.io-client';
 
 // export const SOCKET_URL = 'http://localhost:8000';
-export const SOCKET_URL = 'https://muse-api.com';
+export const SOCKET_URL = process.env.NEXT_PUBLIC_SERVER_URL!;
 
 const socket = io(SOCKET_URL);
 export default socket;
