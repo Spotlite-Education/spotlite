d778aa62dc7f2dd1b5557af3d148c0ef3fe0c291
Andrew Zhou <andrewzhou16@gmail.com>
2024-04-16 15:13:25 -0700

    Fix: Server url

--- src/context/socket.tsx
+++ src/context/socket.tsx
@@ -3,8 +3,7 @@
 import { io } from 'socket.io-client';
 import type { Socket } from 'socket.io-client';
 
-export const SOCKET_URL = 'http://localhost:8000';
-// export const SOCKET_URL = process.env.NEXT_PUBLIC_SERVER_URL!;
+export const SOCKET_URL = process.env.NEXT_PUBLIC_SERVER_URL!;
 
 const socket = io(SOCKET_URL);
 export default socket;
